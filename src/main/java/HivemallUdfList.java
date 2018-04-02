0cdf9013456a943cb9fa9a5f083037561b2504d4
Takuya Kitazawa <k.takuti@gmail.com>
2018-04-03 08:05:42 +0900

    Get @Since and @Deprecated annotation as well

--- src/main/java/HivemallUdfList.java
+++ src/main/java/HivemallUdfList.java
@@ -1,3 +1,4 @@
+import hivemall.annotations.Since;
 import org.reflections.Reflections;
 import org.apache.hadoop.hive.ql.exec.Description;
 
@@ -8,8 +9,23 @@ public class HivemallUdfList {
         Reflections reflections = new Reflections("hivemall");
         Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(Description.class);
         for (Class<?> clazz : annotated) {
+            String s = "";
+
+            Deprecated deprecated = clazz.getAnnotation(Deprecated.class);
+            if (deprecated != null) {
+                s += "[deprecated] ";
+            }
+
+            // FIXME: Since `RetentionPolicy.SOURCE` is set to this annotation, `getAnnotation()` always returns null.
+            Since since = clazz.getAnnotation(Since.class);
+            if (since != null) {
+                s += "[since " + since.version() + "] ";
+            }
+
             Description desc = clazz.getAnnotation(Description.class);
-            System.out.println(desc.name() + " " + desc.value());
+            s += desc.name() + " " + desc.value() + " " + desc.extended();
+
+            System.out.println(s);
         }
     }
 }
